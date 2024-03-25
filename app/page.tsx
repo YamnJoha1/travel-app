d01a0e93bc9d5b09adae27bf3585e9b3641ec7f9
YamnJoha1 <yamn.joha.work@gmail.com>
2024-03-25 20:29:02 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guid from "@/components/Guid";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guid />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
